c2b0659381b70bd50502ec2f61da95e0b581f8db
mordomoy <0000158920726sp@al.educacao.sp.gov.br>
2025-08-21 09:31:58 -0300

    commit

---
+++ s17a3.py
@@ -0,0 +1,16 @@
+num_pessoas = int(input("Qual o num de pessoas: "))
+tipo_reuniao = input("tipo reuniao: ")
+
+if num_pessoas <= 5:
+    print("sala pequena.")
+elif num_pessoas >= 5 and <= 15:
+     print("sala média.")
+elif num_pessoas >=  15:
+
+
+
+
+
+
+
+
