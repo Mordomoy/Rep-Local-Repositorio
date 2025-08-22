4e24d97cc994574036d0d33404c199a833bcba94
mordomoy <00001158920726sp@al.educacao.sp.gov.br>
2025-08-22 08:18:18 -0300

    s17a3

--- s17a3.py
+++ s17a3.py
@@ -1,11 +1,15 @@
-num_pessoas = int(input("Qual o num de pessoas: "))
-tipo_reuniao = input("tipo reuniao: ")
+num_pessoas = int(input("Qual o número de pessoas: "))
+tipo_reuniao = input("Tipo de reunião ('normal' ou 'executiva'): ").lower()
 
-if num_pessoas <= 5:
-    print("sala pequena.")
-elif num_pessoas >= 5 and <= 15:
-     print("sala média.")
-elif num_pessoas >=  15:
+if tipo_reuniao == "executiva" or num_pessoas > 15:
+    print("Sala grande.")
+elif num_pessoas >= 1 and num_pessoas <= 5:
+    print("Sala pequena.")
+elif num_pessoas > 5 and num_pessoas <= 15:
+    print("Sala média.")
+
+
+
 
 
 
